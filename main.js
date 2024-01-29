e84a279d826aa54b95ff1edb2524b01baef16693
Jeevalakshmi94 <76044776+Jeevalakshmi94@users.noreply.github.com>
2024-01-29 06:05:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
-function setup(){
-canvas=createCanvas(300, 300)
-canvas.center()
-background("white")
-canvas.mouseReleased(classifycanvas)
-synt=window.speechSynthesis
-}
-function preload(){
-    classifier=ml5.imageClassifier("DoodleNet")
-}
-function draw(){
-    strokeWeight(3)
-    stroke("black")
-    if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseX)
-    }
-}
-function classifycanvas(){
-    classifier.classify(canvas, gotreuslt)
-}
-function gotreuslt(error, results){
-    if (error) {
-        console.log(error)
-    } else {
-        console.log(results)
-        document.getElementById("label").innerHTML="label:"+results[0].label
-        document.getElementById("confidence").innerHTML="confidence:"+Math.round(results[0].confidence*100)+"%"
-        utter=new SpeechSynthesisUtterance(results[0].label)
-        synt.speak(utter)
-    }
 
+
+function setup() {
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+  synth = window.speechSynthesis;
 }
+
+function preload() {
+
+
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+
+
+function clearCanvas() {
+
+  background("white");
+}
+
+function draw() {
+
+  // Set stroke weight to 13
+  strokeWeight(13);
+  // Set stroke color to black
+  stroke(0);
+  // If mouse is pressed, draw line between previous and current mouse positions
+  if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+}
+
+function classifyCanvas() {
+  classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results);
+  document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+  document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+  utterThis = new SpeechSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
+}
+
